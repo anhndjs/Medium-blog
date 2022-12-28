12eea2d8b106c027f1aa79c24e770d731100d5fc
anhndjs <ducanh01122002@gmail.com>
2022-12-28 16:00:59 +0700

    a

--- app/datasources/utils/redis/stores.js
+++ app/datasources/utils/redis/stores.js
@@ -1,35 +1,3 @@
-// // const redis = require('redis');
-// // const bluebird = require('bluebird');
-
-// // const config = require('../../../config');
-
-// // bluebird.promisifyAll(redis.RedisClient.prototype);
-
-// // const authenticateStore = redis.createClient(config.redisDbs.authDb);
-
-// // module.exports = {
-// //   authenticateStore,
-// // };
-// const Redis = require('ioredis');
-// const { redisDbs } = require('../../../config');
-
-// const redisModel = new Redis({
-//   host: redisDbs.authDb.host,
-//   port: redisDbs.authDb.port,
-//   password: redisDbs.authDb.auth_pass,
-// });
-
-// redisModel.on('connect', () => {
-//   console.log('Redis client connected');
-// });
-// redisModel.on('error', err => {
-//   console.log(`Something went wrong ${err}`);
-// });
-// redisModel.on('shutdown', () => {
-//   console.log('Redis client shutdown');
-// });
-
-// module.exports = redisModel;
 const { createClient } = require('redis');
 
 const redis = createClient();
